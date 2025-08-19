ab67016a6ef0881e0721a5328d500363af351019
Fredrik Zetterman <fredrik.zetterman@rodengames.com>
2025-08-19 16:41:48 +0200

    Now reads toml file with default args

--- src/indie/__main__.py
+++ src/indie/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import logging
 import json
 import pathlib
@@ -9,6 +10,8 @@ import tomlkit
 import argparse
 import pycountry
 import validators
+import tzlocal
+from zoneinfo import available_timezones
 from aiohttp import web
 
 valid_keyboard_layouts = [
@@ -39,13 +42,14 @@ valid_keyboard_layouts = [
     "tr",
 ]
 
-indie_toml_file = "indie.toml"
+indie_toml_file = ".indie/indie.toml"
 indie_toml = tomlkit.document()
 
 
 def write_toml(table: str, data: dict):
     indie_toml[table] = data
 
+    os.makedirs(os.path.dirname(indie_toml_file), exist_ok=True)
     with open(indie_toml_file, "w", encoding="utf-8") as file:
         file.write(indie_toml.as_string())
 
@@ -119,11 +123,36 @@ def get_email(args):
     return selected_email
 
 
+def get_timezone(args):
+    selected_timezone = args.timezone
+
+    while selected_timezone not in available_timezones():
+        if selected_timezone is not None:
+            print(f"{selected_timezone} is not a valid timezone")
+        print("Select timezone:")
+
+        suggested_timezone = str(tzlocal.get_localzone())
+
+        do_suggest = False
+        if suggested_timezone is not None:
+            print(
+                f"(Suggested timezone is {suggested_timezone}, press Enter to accept, or explictly enter a timezone)"
+            )
+            do_suggest = True
+
+        selected_timezone = str(input() or "")
+        if selected_timezone == "" and do_suggest:
+            selected_timezone = suggested_timezone
+    print(f"Selected timezone: {selected_timezone}")
+    return selected_timezone
+
+
 def command_begin(args):
     selected_keyboard_layout = get_keyboard(args)
     selected_countrycode = get_countrycode(args)
     selected_domain = get_domain(args)
     selected_email = get_email(args)
+    selected_timezone = get_timezone(args)
     write_toml(
         "indie",
         {
@@ -132,6 +161,7 @@ def command_begin(args):
                 "countrycode": selected_countrycode,
                 "domain": selected_domain,
                 "email": selected_email,
+                "timezone": selected_timezone,
             }
         },
     )
@@ -159,7 +189,22 @@ def validate_countrycode(value):
     return value
 
 
+def get_toml_default(key):
+    try:
+        return indie_toml["indie"]["global"][key]
+    except KeyError:
+        return None
+
+
 def main():
+    # Read default values, if possible
+    try:
+        with open(indie_toml_file, "r", encoding="utf-8") as file:
+            global indie_toml
+            indie_toml = tomlkit.load(file)
+    except FileNotFoundError:
+        pass
+
     parser = argparse.ArgumentParser()
     parser.set_defaults(func=(lambda args, p=parser: command_unknown(args, p)))
     subparsers = parser.add_subparsers(help="Available subcommands")
@@ -169,16 +214,20 @@ def main():
         "begin", help="Begin the initial setup of the infrastructure"
     )
     p_begin.add_argument(
-        "--keyboard", choices=valid_keyboard_layouts, help="Keyboard layout to use."
+        "--keyboard",
+        choices=valid_keyboard_layouts,
+        help="Keyboard layout to use.",
+        default=get_toml_default("keyboard"),
     )
     p_begin.add_argument(
         "--countrycode",
         type=validate_countrycode,
         help="ISO 3166-1 alpha 2 countrycode to use (for example DE, FR, GB, or SE). See https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2 for full list.",
+        default=get_toml_default("countrycode"),
     )
-    p_begin.add_argument("--domain")
-    p_begin.add_argument("--email")
-    p_begin.add_argument("--timezone")
+    p_begin.add_argument("--domain", default=get_toml_default("domain"))
+    p_begin.add_argument("--email", default=get_toml_default("email"))
+    p_begin.add_argument("--timezone", default=get_toml_default("timezone"))
     p_begin.set_defaults(func=command_begin)
 
     # addhost
@@ -188,12 +237,6 @@ def main():
     p_addhost.set_defaults(func=command_addhost)
 
     args = parser.parse_args()
-
-    try:
-        with open(indie_toml_file, "r", encoding="utf-8") as file:
-            indie_toml = tomlkit.load(file)
-    except FileNotFoundError:
-        pass
 
     args.func(args)
 
